98598974fc5cf3aa243bcd15947529f2bee1eed6
w00452607 <wangyouxiang2@huawei.com>
2021-02-16 23:53:26 +0800

    dct.py

--- 01-dft/dct.py
+++ 01-dft/dct.py
@@ -1,25 +1,43 @@
+import math
+
 import numpy as np
-import matplotlib.pyplot as plt
-import cv2
-from scipy.fftpack import dct, idct,fft
-
-def my_dct(a):
-    # a'[m] = a[m]       0<=m<=N-1
-    # a'[m] = a[-m-1]    -N<=m<=-1
-    assert a.ndim == 1
-    
+from scipy.fftpack import dct, idct
 
 
+def my_dct(x):
+    assert x.ndim == 1
+    size = x.shape[0]
+    res = np.zeros(size)
+    for i in range(size):
+        for j in range(size):
+            if i == 0:
+                res[i] += math.sqrt(1.0 / size) * x[j] * math.cos((2 * j + 1) * math.pi * i / (2 * size))
+            else:
+                res[i] += math.sqrt(2.0 / size) * x[j] * math.cos((2 * j + 1) * math.pi * i / (2 * size))
+    return res
 
 
+def my_idct(x):
+    assert x.ndim == 1
+    size = x.shape[0]
+    res = np.zeros(size)
+    for i in range(size):
+        for j in range(size):
+            if j == 0:
+                res[i] += math.sqrt(1.0 / size) * x[j] * math.cos((2 * i + 1) * math.pi * j / (2 * size))
+            else:
+                res[i] += math.sqrt(2.0 / size) * x[j] * math.cos((2 * i + 1) * math.pi * j / (2 * size))
+    return res
 
 
+if __name__ == '__main__':
+    x = np.array([1, 2, 3, 5, 7])
+    dct2 = dct(x, norm='ortho')
+    idct2 = idct(dct2, norm='ortho')
+    print("dct2", dct2)
+    print("idct2", idct2)
 
-if __name__=='__main__':
-    a = np.array([1,2])
-    print(a.ndim)
-    a_dct = dct(a,norm='ortho')
-    a_dct2 = dct(a)
-    print(a_dct)
-    print(a_dct2)
-    print(a_dct2/a_dct)
+    my_dct2 = my_dct(x)
+    my_idct2 = my_idct(my_dct2)
+    print("my_dct2", my_dct2)
+    print("my_idct2", my_idct2)
